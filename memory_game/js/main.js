a65f0291f0767767c3d382297348c08e033809a9
Stuart Potter <stuart@Stuarts-MacBook-Air.local>
2017-09-12 13:33:28 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,71 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	if(cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+	}
+};
+
+
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+};
+
+flipCard(0);
+flipCard(1);
+
+
+
+
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+
+// if(cardsInPlay.length === 2) {
+// 	if(cardsInPlay[0] === cardsInPlay[1]) {
+// 		alert("You found a match!");
+// 	} else {
+// 		alert("Sorry, try again.");
+// 	}
+// }
+
+
+
+
+
 
 
