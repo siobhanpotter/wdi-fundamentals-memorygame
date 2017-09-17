e348fb70471990a0c6a505c9dead2f9587403d1c
Stuart Potter <stuart@Stuarts-MacBook-Air.local>
2017-09-17 14:17:46 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,10 +23,18 @@ cardImage: "images/king-of-diamonds.png"
 ];
 
 var cardsInPlay = [];
+var newPoints = []
+
+var totalScore = function() {
+	newPoints + newPoints
+}
 
 var checkForMatch = function() {
 		if(cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
+			alert("You found a match! 10points!");
+			newPoints.push(10);
+			console.log("Well done you got " + newPoints + " points!");
+			totalScore();
 		} else {
 			alert("Sorry, try again.");
 		}
@@ -42,11 +50,14 @@ var flipCard = function() {
 	cardsInPlay.push(cards[cardId].rank);
 	this.setAttribute("src", cards[cardId].cardImage);
 
+
+
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 
 };
+
 
 
 var createBoard = function() {
@@ -61,6 +72,27 @@ var createBoard = function() {
 
 createBoard();
 
+
+//I started trying to reset the board but havn't found a solution yet, I also started to figure out ,
+//how to add points but also havn't finished this.
+
+//Attempt1 at reseting cards! - this just adds more cards to the page, but the flipped cards are still visible,
+//Could I hide the original cards?
+
+var recreateBoard = function() {
+	createBoard();	
+}
+
+var resetBoard = function() {
+	var resetButton = document.getElementById('myButton')
+	resetButton.addEventListener('click', recreateBoard);
+}
+
+ resetBoard();
+
+// Other ideas - soemhow loop back to before the section of code where the cards are flipped.
+
+// or When the reset button is clicked the cards need to flip back to show the back of the cards.
 
 
 
